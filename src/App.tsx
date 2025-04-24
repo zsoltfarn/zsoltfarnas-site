16ef2c89476749f903acc82576bd65557ee333c4
Zsolt Farnas <zsolt.farnas@gmail.com>
2025-04-24 19:04:20 +0200

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // React import can be removed since it's not directly used in this file
 // First install react-router-dom using: npm install react-router-dom @types/react-router-dom
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import * as reactRouterDom from 'react-router-dom';
 // If the error persists, run: npm install react-router-dom @types/react-router-dom --save
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
@@ -10,15 +10,9 @@ import Pricing from './components/Pricing';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import ScrollToTopButton from './components/ScrollToTopButton';
-// Create and import Home component once implemented
-// import Home from './pages/Home';
-const Home = () => <div>Home Page</div>; // Temporary component until proper Home page is created
-import Success from './pages/Success'; // Import the new Success component
-// Remove unused import since ContactForm is not currently used in the component
-
 function App() {
   return (
-    <Router>
+    <reactRouterDom.BrowserRouter>
       <Navbar />
       <Hero />
       <Services />
@@ -27,21 +21,8 @@ function App() {
       <Contact />
       <Footer />
       <ScrollToTopButton />
-      <Routes>
-        {/* Assuming your main page route */}
-        <Route path="/" element={<Home />} />
-
-        {/* Add the route for the success page */}
-        <Route path="/success" element={<Success />} />
-
-        {/* You might have other routes */}
-        {/* If ContactForm is meant to be on its own page, add a route for it */}
-        {/* <Route path="/contact" element={<ContactForm />} /> */}
-
-        {/* Or if ContactForm is part of another page (like Home), it doesn't need its own route */}
-
-      </Routes>
-    </Router>
+      
+    </reactRouterDom.BrowserRouter>
   );
 }
 
