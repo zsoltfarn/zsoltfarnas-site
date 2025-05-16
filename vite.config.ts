2904469dd858cd265b3e90d1d9f8f96c3b9bfc36
Zsolt Farnas <zsolt.farnas@gmail.com>
2025-05-16 19:48:33 +0200

    docs(vite.config): clarify comment for base path configuration
    The comment for the base path configuration was updated to be more descriptive and clear about its purpose

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // 👈 This is required for subpath on Netlify
+  base: '/', // Set the base path to '/'
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
